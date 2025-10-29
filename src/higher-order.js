efd9ff6ca09dc0514c6232d6f30d2ab2ac49c921
Mattias <128178112+Matzy91@users.noreply.github.com>
2025-10-29 19:46:24 +0100

    done

--- src/higher-order.js
+++ src/higher-order.js
@@ -4,32 +4,38 @@ import { personArray, emojiArray } from "./data.js";
 // med undantag för "getObjectFromArray" nederst som redan är en funktion
 
 //personArray
-export const namesOnly;
-export const agesOnly;
-export const withoutJim;
-export const combinedAge;
-export const findJane;
+export const namesOnly = personArray.map(p => p.name);
+export const agesOnly = personArray.map(p => p.age);
+export const withoutJim = personArray.filter(p => p.name !=="Jim");
+export const combinedAge = personArray.reduce((sum, p) => sum + p.age, 0);
+export const findJane = personArray.find(p => p.name === "Jane");
 
-export const sortedByAgeFromYoungestToOldest;
-export const namesByAgeFromOldestToYoungest;
-export const everyoneOver12;
-export const isAnyoneOver13;
+export const sortedByAgeFromYoungestToOldest = [...personArray].sort((a, b) => a.age - b.age);
+export const namesByAgeFromOldestToYoungest = [...personArray]
+  .sort((a, b) => b.age - a.age)
+  .map(p => p.name);
 
-export const indexOfMaybe;
-export const namesByLastLetter;
+export const everyoneOver12 = personArray.every(p => p.age > 12);
+export const isAnyoneOver13 = personArray.some(p => p.age > 13);
 
-/* ---------------------------------- */
+export const indexOfMaybe = personArray.findIndex(p => p.city === "Maybe");
+export const namesByLastLetter = personArray.map(p => p.name.slice(-1));
 
-// emojiArray
-export const arrayIncludesSquirel;
-export const indexOfRocket;
+// /* ---------------------------------- */
+
+// // emojiArray
+export const arrayIncludesSquirel = emojiArray.includes("🐿️");
+export const indexOfRocket = emojiArray.indexOf("🚀");
 
 /* ---------------------------------- */
 
 // ska skapa ett objekt innehållande objekten i personArray
 const getObjectFromArray = () => {
-  const newObject = {};
-  return newObject;
+  return personArray.reduce((acc, person) => {
+    const key = person.name.toLowerCase();
+    acc[key] = person;
+    return acc;
+  }, {});
 };
 
 // lämnas orörda. dessa två testar funktionaliteten
